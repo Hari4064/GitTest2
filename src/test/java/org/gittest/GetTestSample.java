41f2fe54041e350f7e369e336cc33930db35b86a
Hari4064 <hari4064@gmail.com>
2022-03-11 23:46:59 +0530

    project2

---
+++ src/test/java/org/gittest/GetTestSample.java
@@ -0,0 +1,18 @@
+package org.gittest;
+
+
+
+public class GetTestSample {
+	private void test1() {
+		System.out.println("test");
+
+	}
+	private void test2() {
+		System.out.println("test2");
+	}
+	private void test3() {
+        System.out.println("test3");
+
+	}
+	
+}
